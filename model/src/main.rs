5b4895842c61ec109d3ae36ee20c0a53c0a13923
Adin Ackerman <adin@fourier.earth>
2026-05-11 10:40:34 -0700

    validation exit code

--- model/src/main.rs
+++ model/src/main.rs
@@ -1,9 +1,17 @@
+use std::process::ExitCode;
+
 use cortex_m_spa_model::compose;
 use enum_iterator::all;
 
-fn main() {
+fn main() -> ExitCode {
+    let mut exit_code = ExitCode::SUCCESS;
+
     for device in all() {
         println!("=== Variant: {device:?} ===");
-        phm::validate(compose(Some(device)));
+        if ExitCode::FAILURE == phm::validate(compose(Some(device))) {
+            exit_code = ExitCode::FAILURE;
+        }
     }
+
+    exit_code
 }
